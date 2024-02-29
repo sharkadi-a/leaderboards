db85fc6be45f49cbfeb66ab3a421774740791ef3
Sharkadi Andrey <sharkadi.a@gmail.com>
2024-03-01 00:54:46 +0100

    Migration for insensitive player name search

---
+++ src/AndreyGames.Leaderboards.Service/Migrations/20240229225828_AddCaseInsensitiveIndex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AndreyGames.Leaderboards.Service.Migrations
+{
+    public partial class AddCaseInsensitiveIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE INDEX IX_Entries_LeaderboardId_LowerPlayerName ON \"Entries\" (\"LeaderboardId\", LOWER(\"PlayerName\"));");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Entries_LeaderboardId_LowerPlayerName",
+                table: "Entries");
+        }
+    }
+}
